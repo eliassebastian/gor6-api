d18dc044c7c4d1decb2b175b76039c7f7fbeb5db
Elias Sebastian <eeschmoelz@gmail.com>
2021-12-16 21:31:54 +0000

    weapon + operator statistics

---
+++ cmd/api/models/operator.go
@@ -0,0 +1,171 @@
+package models
+
+type OperatorModel struct {
+	ProfileID string            `json:"profileId"`
+	Platforms OperatorPlatforms `json:"platforms"`
+}
+
+/*
+type KillDeathRatio struct {
+	Value float64 `json:"value"`
+	P     int     `json:"p"`
+}
+
+type HeadshotAccuracy struct {
+	Value float64 `json:"value"`
+	P     int     `json:"p"`
+}
+
+type KillsPerRound struct {
+	Value int `json:"value"`
+	P     int `json:"p"`
+}
+
+type RoundsWithAKill struct {
+	Value float64 `json:"value"`
+	P     int     `json:"p"`
+}
+
+type RoundsWithMultiKill struct {
+	Value float64 `json:"value"`
+	P     int     `json:"p"`
+}
+
+type RoundsWithOpeningKill struct {
+	Value float64 `json:"value"`
+	P     int     `json:"p"`
+}
+
+type RoundsWithOpeningDeath struct {
+	Value float64 `json:"value"`
+	P     int     `json:"p"`
+}
+
+type RoundsWithKOST struct {
+	Value float64 `json:"value"`
+	P     int     `json:"p"`
+}
+
+type RoundsSurvived struct {
+	Value float64 `json:"value"`
+	P     int     `json:"p"`
+}
+
+type RoundsWithAnAce struct {
+	Value int `json:"value"`
+	P     int `json:"p"`
+}
+
+type RoundsWithClutch struct {
+	Value int `json:"value"`
+	P     int `json:"p"`
+}
+*/
+type Attacker struct {
+	//Type      string `json:"type"`
+	//StatsType string `json:"statsType"`
+	StatsDetail   string `json:"statsDetail"`
+	MatchesPlayed int    `json:"matchesPlayed"`
+	RoundsPlayed  int    `json:"roundsPlayed"`
+	MinutesPlayed int    `json:"minutesPlayed"`
+	MatchesWon    int    `json:"matchesWon"`
+	MatchesLost   int    `json:"matchesLost"`
+	RoundsWon     int    `json:"roundsWon"`
+	RoundsLost    int    `json:"roundsLost"`
+	Kills         int    `json:"kills"`
+	Assists       int    `json:"assists"`
+	Death         int    `json:"death"`
+	Headshots     int    `json:"headshots"`
+	MeleeKills    int    `json:"meleeKills"`
+	//TeamKills              int                    `json:"teamKills"`
+	//OpeningKills           int                    `json:"openingKills"`
+	//OpeningDeaths          int                    `json:"openingDeaths"`
+	//Trades                 int                    `json:"trades"`
+	//OpeningKillTrades      int                    `json:"openingKillTrades"`
+	//OpeningDeathTrades     int                    `json:"openingDeathTrades"`
+	//Revives                int                    `json:"revives"`
+	//DistanceTravelled      int                    `json:"distanceTravelled"`
+	WinLossRatio int `json:"winLossRatio"`
+	//KillDeathRatio         KillDeathRatio         `json:"killDeathRatio"`
+	//HeadshotAccuracy       HeadshotAccuracy       `json:"headshotAccuracy"`
+	//KillsPerRound          KillsPerRound          `json:"killsPerRound"`
+	//RoundsWithAKill        RoundsWithAKill        `json:"roundsWithAKill"`
+	//RoundsWithMultiKill    RoundsWithMultiKill    `json:"roundsWithMultiKill"`
+	//RoundsWithOpeningKill  RoundsWithOpeningKill  `json:"roundsWithOpeningKill"`
+	//RoundsWithOpeningDeath RoundsWithOpeningDeath `json:"roundsWithOpeningDeath"`
+	//RoundsWithKOST         RoundsWithKOST         `json:"roundsWithKOST"`
+	//RoundsSurvived         RoundsSurvived         `json:"roundsSurvived"`
+	//RoundsWithAnAce        RoundsWithAnAce        `json:"roundsWithAnAce"`
+	//RoundsWithClutch       RoundsWithClutch       `json:"roundsWithClutch"`
+	TimeAlivePerMatch float64 `json:"timeAlivePerMatch"`
+	TimeDeadPerMatch  float64 `json:"timeDeadPerMatch"`
+	DistancePerRound  float64 `json:"distancePerRound"`
+}
+
+type Defender struct {
+	//Type      string `json:"type"`
+	//StatsType string `json:"statsType"`
+	StatsDetail   string `json:"statsDetail"`
+	MatchesPlayed int    `json:"matchesPlayed"`
+	RoundsPlayed  int    `json:"roundsPlayed"`
+	MinutesPlayed int    `json:"minutesPlayed"`
+	MatchesWon    int    `json:"matchesWon"`
+	MatchesLost   int    `json:"matchesLost"`
+	RoundsWon     int    `json:"roundsWon"`
+	RoundsLost    int    `json:"roundsLost"`
+	Kills         int    `json:"kills"`
+	Assists       int    `json:"assists"`
+	Death         int    `json:"death"`
+	Headshots     int    `json:"headshots"`
+	MeleeKills    int    `json:"meleeKills"`
+	//TeamKills              int                    `json:"teamKills"`
+	//OpeningKills           int                    `json:"openingKills"`
+	//OpeningDeaths          int                    `json:"openingDeaths"`
+	//Trades                 int                    `json:"trades"`
+	//OpeningKillTrades      int                    `json:"openingKillTrades"`
+	//OpeningDeathTrades     int                    `json:"openingDeathTrades"`
+	//Revives                int                    `json:"revives"`
+	//DistanceTravelled      int                    `json:"distanceTravelled"`
+	WinLossRatio int `json:"winLossRatio"`
+	//KillDeathRatio         KillDeathRatio         `json:"killDeathRatio"`
+	//HeadshotAccuracy       HeadshotAccuracy       `json:"headshotAccuracy"`
+	//KillsPerRound          KillsPerRound          `json:"killsPerRound"`
+	//RoundsWithAKill        RoundsWithAKill        `json:"roundsWithAKill"`
+	//RoundsWithMultiKill    RoundsWithMultiKill    `json:"roundsWithMultiKill"`
+	//RoundsWithOpeningKill  RoundsWithOpeningKill  `json:"roundsWithOpeningKill"`
+	//RoundsWithOpeningDeath RoundsWithOpeningDeath `json:"roundsWithOpeningDeath"`
+	//RoundsWithKOST         RoundsWithKOST         `json:"roundsWithKOST"`
+	//RoundsSurvived         RoundsSurvived         `json:"roundsSurvived"`
+	//RoundsWithAnAce        RoundsWithAnAce        `json:"roundsWithAnAce"`
+	//RoundsWithClutch       RoundsWithClutch       `json:"roundsWithClutch"`
+	TimeAlivePerMatch float64 `json:"timeAlivePerMatch"`
+	TimeDeadPerMatch  float64 `json:"timeDeadPerMatch"`
+	DistancePerRound  float64 `json:"distancePerRound"`
+}
+
+type OperatorTeamRoles struct {
+	Attacker []Attacker `json:"attacker"`
+	Defender []Defender `json:"defender"`
+}
+
+type OperatorGameMode struct {
+	//Type      string    `json:"type"`
+	TeamRoles OperatorTeamRoles `json:"teamRoles"`
+}
+
+type OperatorGameModes struct {
+	All      OperatorGameMode `json:"all"`
+	Casual   OperatorGameMode `json:"casual"`
+	Ranked   OperatorGameMode `json:"ranked"`
+	Unranked OperatorGameMode `json:"unranked"`
+}
+
+type OperatorPlatform struct {
+	GameModes OperatorGameModes `json:"gameModes"`
+}
+
+type OperatorPlatforms struct {
+	Pc   OperatorPlatform `json:"PC,omitempty"`
+	Xbox OperatorPlatform `json:"XBOX,omitempty"`
+	Ps4  OperatorPlatform `json:"PS4,omitempty"`
+}
