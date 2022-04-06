f9197418b4f78eaab6722d5b63cf7fac47af6304
Elias Sebastian <eeschmoelz@gmail.com>
2022-04-06 16:58:43 +0100

    delete and create new elasticsearch indices on server start (dev only)

--- internal/elastic/elasticsearch.go
+++ internal/elastic/elasticsearch.go
@@ -3,6 +3,7 @@ package elastic
 import (
 	"errors"
 	"github.com/elastic/go-elasticsearch/v7"
+	"log"
 )
 
 type ESClient struct {
@@ -25,6 +26,22 @@ func NewElasticClient() (*ESClient, error) {
 	res, err := es.Info()
 	if err != nil {
 		return nil, errors.New("error pinging new elasticsearch client")
+	}
+
+	//DEV ONLY
+	for _, index := range []string{"r6index.uplay", "r6index.psn", "r6index.xbl"} {
+		if res, err = es.Indices.Delete([]string{index}); err != nil {
+			log.Fatalf("Cannot delete index: %s", err)
+		}
+
+		res, err = es.Indices.Create(index)
+		if err != nil {
+			log.Fatalf("Cannot create index: %s", err)
+		}
+
+		if res.IsError() {
+			log.Fatalf("Cannot create index: %s", res)
+		}
 	}
 
 	res.Body.Close()
